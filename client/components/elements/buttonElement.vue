accdfaf4d0b9916e9e8e61a384f4382e0a3155dd
golubvladimir <golubvlauri@gmail.com>
2021-06-11 00:26:03 +0200

    add button element

---
+++ client/components/elements/buttonElement.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="h-50px bg-indigo-600 text-white">
+    <slot />
+  </button>
+</template>
+
+<script>
+  export default {
+    name: "button-element"
+  }
+</script>
+
+<style scoped>
+
+</style>
