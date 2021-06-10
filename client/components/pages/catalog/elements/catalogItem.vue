cbc7a9aaea286d4c639b15cbed9d42ce6300f7e4
golubvladimir <golubvlauri@gmail.com>
2021-06-11 00:22:10 +0200

    add catalog page

---
+++ client/components/pages/catalog/elements/catalogItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="">
+    <img
+      :src="img"
+      :alt="name"
+    >
+    <p>{{ name }}</p>
+    <p>{{ price }}</p>
+  </div>
+</template>
+
+<script>
+import {defineComponent, toRefs} from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  setup(props) {
+    const { name, price, img } = toRefs(props);
+
+    return {
+      img,
+      name,
+      price
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/components/pages/catalog/sections/catalogList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="">
+    <catalog-item
+      v-for="item in items"
+      class=""
+      :img="item.img"
+      :name="item.name"
+      :price="item.price"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, toRefs} from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  components: {
+    catalogItem: () => import('@/components/pages/catalog/elements/catalogItem')
+  },
+  setup(props) {
+    const { items } = toRefs(props);
+
+    return {
+      items
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
