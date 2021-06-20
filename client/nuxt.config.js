e1bbe839b614417455a22e49b136387e15a3613c
golubvladimir <golubvlauri@gmail.com>
2021-06-20 23:27:26 +0200

    add axios settings

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -21,6 +21,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '@/plugins/axios-accessor.ts' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -41,5 +42,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  // axios module
+  axios: {
+    baseURL: 'http://localhost:3001/api',
+    browserBaseURL: 'http://localhost:3001/api'
   }
 }
